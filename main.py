b4376ecab9e12ca724dd07c237fac4e2e9f7cb40
Kridi8606 <www.fedakridi.ru@gmail.com>
2023-03-13 15:48:37 +0300

    Тренировки в полёте

--- main.py
+++ main.py
@@ -10,5 +10,16 @@ def index(title):
         return f.read().replace('{{ title }}', title).replace('{{ style_main }}', url_for('static', filename='css/style_main.css'))
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    with open('templates/training.html', 'r', encoding='utf-8') as f:
+        if 'строитель' in prof.lower():
+            prof = 'строитель'
+        elif 'инженер' in prof.lower():
+            prof = 'инженер'
+        img = prof + '_img.png'
+        return f.read().replace('{{ title }}', prof).replace('{{ style_main }}', url_for('static', filename='css/style_main.css')).replace('{{ image }}', url_for('static', filename='img/' + img))
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
